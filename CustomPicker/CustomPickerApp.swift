af440cebde8426615755ce9d476073f2a9621c3c
berkahhariini999-rgb <berkahhariini999@gmail.com>
2025-12-30 09:38:35 +0700

    Initial Commit

---
+++ CustomPicker/CustomPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPickerApp.swift
+//  CustomPicker
+//
+//  Created by Iqbal Alhadad on 30/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
